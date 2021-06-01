c90a266daa9710374ad27e730d64c963c424f942
GabrielArsenio <gabrielarsenioo@gmail.com>
2021-06-01 11:50:46 -0300

    Adicionado banner com campo de busca

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <div
+      class="banner d-flex align-items-center flex-column justify-content-center"
+    >
+      <h1>Entrega em 2h!</h1>
+      <span>Tenha sua comida na porta da sua casa</span>
+      <div class="input-group mt-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Buscar por item ou marca"
+          aria-label="Buscar por item ou marca"
+          aria-describedby="button-addon2"
+        />
+        <button
+          class="btn btn-primary"
+          type="button"
+          id="button-addon2"
+        >
+          Buscar
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,6 +29,15 @@
 // @ is an alias to /src
 
 export default {
-  name: 'Home'
-}
+  name: "Home",
+};
 </script>
+
+<style scoped>
+.banner {
+  height: 35vh;
+  background-image: url("../assets/banner.png");
+  background-size: cover;
+  background-position: center;
+}
+</style>
