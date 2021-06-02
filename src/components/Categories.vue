c7ae2ab7332b8796e21ed11cd683839ed8f3e96c
GabrielArsenio <gabrielarsenioo@gmail.com>
2021-06-01 21:12:58 -0300

    Criada Categorias

---
+++ src/components/Categories.vue
@@ -0,0 +1,132 @@
+<template>
+  <ul>
+    <li v-for="category of categories" :key="category.id">
+      <img
+        :src="category.img"
+        class="rounded-circle"
+        height="80"
+        alt="Category"
+      />
+      <div class="name-container">
+        <span>{{ category.name }}</span>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Categories",
+  data() {
+    return {
+      categories: [
+        {
+          id: 1,
+          img:
+            "https://static-images.ifood.com.br/image/upload//mercados/categorias/carne.png",
+          name: "Carnes, Aves e Peixes",
+        },
+        {
+          id: 1,
+          img:
+            "https://static-images.ifood.com.br/image/upload//mercados/categorias/basicos.png",
+          name: "Alimentos Básicos",
+        },
+        {
+          id: 1,
+          img:
+            "https://static-images.ifood.com.br/image/upload//mercados/categorias/bebidas.png",
+          name: "Bebidas",
+        },
+        {
+          id: 1,
+          img:
+            "https://static-images.ifood.com.br/image/upload//mercados/categorias/bebidas_alcool.png",
+          name: "Bebidas Alcoólicas",
+        },
+        {
+          id: 1,
+          img:
+            "https://static-images.ifood.com.br/image/upload//mercados/categorias/congelados.png",
+          name: "Congelados",
+        },
+        {
+          id: 1,
+          img:
+            "https://static-images.ifood.com.br/image/upload//mercados/categorias/feira.png",
+          name: "Feira",
+        },
+        {
+          id: 1,
+          img:
+            "https://static-images.ifood.com.br/image/upload//mercados/categorias/frios.png",
+          name: "Frios e Laticínios",
+        },
+        {
+          id: 1,
+          img:
+            "https://static-images.ifood.com.br/image/upload//mercados/categorias/leite.png",
+          name: "Leites e Iogurtes",
+        },
+        {
+          id: 1,
+          img:
+            "https://static-images.ifood.com.br/image/upload//mercados/categorias/matinais.png",
+          name: "Matinais",
+        },
+        {
+          id: 1,
+          img:
+            "https://static-images.ifood.com.br/image/upload//mercados/categorias/padaria.png",
+          name: "Padaria",
+        },
+        {
+          id: 1,
+          img:
+            "https://static-images.ifood.com.br/image/upload//mercados/categorias/biscoitos.png",
+          name: "Biscoitos e Salgadinhos",
+        },
+        {
+          id: 1,
+          img:
+            "https://static-images.ifood.com.br/image/upload//mercados/categorias/doces.png",
+          name: "Doces e Sobremesas",
+        },
+        {
+          id: 1,
+          img:
+            "https://static-images.ifood.com.br/image/upload//mercados/categorias/limpeza.png",
+          name: "Limpeza",
+        },
+        {
+          id: 1,
+          img:
+            "https://static-images.ifood.com.br/image/upload//mercados/categorias/higiene.png",
+          name: "Higiene e Cuidados Pessoais",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  display: flex;
+  overflow-y: auto;
+  padding: 20px;
+}
+
+li {
+  padding: 0 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.name-container {
+  text-align: center;
+  font-size: 0.75em;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="home">
-    <div
-      class="banner d-flex align-items-center flex-column justify-content-center"
+  <main class="home">
+    <section
+      id="banner"
+      class="d-flex align-items-center flex-column justify-content-center"
     >
       <h1>Entrega em 2h!</h1>
       <span>Tenha sua comida na porta da sua casa</span>
@@ -13,28 +14,31 @@
           aria-label="Buscar por item ou marca"
           aria-describedby="button-addon2"
         />
-        <button
-          class="btn btn-primary"
-          type="button"
-          id="button-addon2"
-        >
+        <button class="btn btn-primary" type="button" id="button-addon2">
           Buscar
         </button>
       </div>
-    </div>
-  </div>
+    </section>
+    <section id="categories">
+      <Categories />
+    </section>
+  </main>
 </template>
 
 <script>
 // @ is an alias to /src
+import Categories from "@/components/Categories";
 
 export default {
   name: "Home",
+  components: {
+    Categories,
+  },
 };
 </script>
 
 <style scoped>
-.banner {
+#banner {
   height: 35vh;
   background-image: url("../assets/banner.png");
   background-size: cover;
